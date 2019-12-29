d3ffcbd252ecc09768c14153f12e53d103f1b439
kojoma <masaru.kojo@gmail.com>
2019-12-29 17:45:16 +0900

    Tweak disabled state on submit button

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -44,7 +44,7 @@ class EventsShow extends Component {
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props;
+    const { handleSubmit, pristine, submitting, invalid } = this.props;
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -54,7 +54,7 @@ class EventsShow extends Component {
         </div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
