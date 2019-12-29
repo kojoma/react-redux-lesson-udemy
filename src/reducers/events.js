c6c849668b84d5ec9b8e6b7854a5ea968f5bc697
kojoma <masaru.kojo@gmail.com>
2019-12-29 17:13:07 +0900

    Make deletable event

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,5 +1,5 @@
 import { mapKeys } from 'lodash';
-import { READ_EVENTS } from "../actions";
+import { READ_EVENTS, DELETE_EVENT } from "../actions";
 
 const initialState = {};
 
@@ -7,6 +7,10 @@ export default (events = initialState, action) => {
   switch (action.type) {
     case READ_EVENTS: {
       return mapKeys(action.response.data, 'id');
+    }
+    case DELETE_EVENT: {
+      delete events[action.id];
+      return { ...events };
     }
     default:
       return events;
